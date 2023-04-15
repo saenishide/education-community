4c1ac17b94e20db082459779aa13537895a07fb3
saenishide <nishide.even@gmail.com>
2023-04-15 11:34:43 +0900

    新規作成

---
+++ educom/src/main/java/com/education/community/EducomApplication.java
@@ -0,0 +1,13 @@
+package com.education.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducomApplication.class, args);
+	}
+
+}
---
+++ educom/src/test/java/com/education/community/EducomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
